b53152e7a4a2a334a0255906a1d9d8c280d5c09c
SofiaPia <96252390+SofiaPia@users.noreply.github.com>
2022-03-11 18:37:04 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/** TYPEWRITER EFFECT **/
+
+var messageArray = ["Front-End Developer"];
+
+var textPosition = 0;
+
+var speed = 100;
+
+typewriter = () => {
+  document.querySelector("#message").
+  innerHTML = messageArray[0].substring(0, textPosition);
+
+  if(textPosition++ != messageArray[0].lenght)
+    setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter);
+
+
+/** LANGUAGES BARS **/
+  
+  var offsetTop = $('#languages').offset().top;
+  
+  $(window).scroll(function() {
+  var height = $(window).height();
+    if($(window).scrollTop()+height > offsetTop) {
+      jQuery('.skillbar').each(function(){
+        jQuery(this).find('.skillbar-bar').animate({
+          width:jQuery(this).attr('data-percent')
+        },2000);
+      });
+    }
+  });
